bee2e25103d2ec6f6862766b5bdcc675c480dcb5
Khaerunisa S. Dewi <137818610+nisasinta06@users.noreply.github.com>
2026-06-08 09:26:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+function showPage(page){
+
+    document
+    .querySelectorAll(".page")
+    .forEach(el=>{
+        el.classList.remove("active");
+    });
+
+    document
+    .getElementById("page"+page)
+    .classList.add("active");
+
+}
+
+/* confetti awal */
+
+window.onload=()=>{
+
+    confetti({
+        particleCount:180,
+        spread:120,
+        origin:{y:0.6}
+    });
+
+}
+
+/* amplop */
+
+document
+.getElementById("envelope")
+.addEventListener("click",function(){
+
+    this.classList.toggle("open");
+
+});
+
+/* hadiah */
+
+document
+.getElementById("gift")
+.addEventListener("click",()=>{
+
+    document
+    .getElementById("gift")
+    .style.display="none";
+
+    document
+    .getElementById("polaroid-container")
+    .style.display="block";
+
+    confetti({
+        particleCount:250,
+        spread:180
+    });
+
+    const photos =
+    document.querySelectorAll(".polaroid");
+
+    photos.forEach((photo,index)=>{
+
+        setTimeout(()=>{
+
+            photo.classList.add("show");
+
+        },index*1000);
+
+    });
+
+    setTimeout(()=>{
+
+        document
+        .getElementById("ending")
+        .style.display="block";
+
+    },5000);
+
+});
